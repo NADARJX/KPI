26a3f7e9a0923c2939d5264671b5a0640c4e8d41
NADARJX <jenita.nadar@abbott.com>
2025-06-10 10:06:42 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 warnings.filterwarnings("ignore")
-st.set_page_config(page_title="KPI Dashboard", page_icon="📊", layout="wide")
+
 
 # -------------------- USER AUTHENTICATION --------------------
 user_roles = {
