e6199fb4fc29510587df99401f2a4b41cd590fa4
NADARJX <jenita.nadar@abbott.com>
2025-06-13 21:54:43 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -48,7 +48,7 @@ if not st.session_state.authenticated:
 # Load data
 ##file_path = r"C:\Users\NADARJX\OneDrive - Abbott\Documents\New folder\KPI new- May 2025.xlsx"
 ##df = pd.read_excel(file_path)
-url = "https://raw.githubusercontent.com/username/repository/main/file.xlsx"
+url = "https://github.com/NADARJX/KPI/blob/main/KPI%20new-%20May%202025.xlsx"
 df = pd.read_excel(url)
 
 
