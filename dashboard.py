975a5d7213996390a604e6fcfd56094176a7ec71
NADARJX <jenita.nadar@abbott.com>
2025-06-14 12:37:07 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -50,7 +50,7 @@ if not st.session_state.authenticated:
 ##df = pd.read_excel(file_path)
 import requests
 
-url = "https://github.com/NADARJX/KPI/blob/main/KPI%20new-%20May%202025.xlsx"
+url = "https://github.com/NADARJX/KPI/blob/main/KPI%20new-%20Jun%202025.csv"
 response = requests.get(url)
 
 with open("temp.xlsx", "wb") as f:
